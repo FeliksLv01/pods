45342e18b41896a51b721ffcdea226f298d30f1e
FeliksLv <felikslv@163.com>
2024-04-25 02:16:47 +0800

    feat: 添加push命令

--- src/command/push.ts
+++ src/command/push.ts
@@ -1,1 +1,24 @@
-export default function push() {}
+import chalk from 'chalk'
+import ora from 'ora'
+import { spawn } from 'child_process'
+
+const push = (name: string) => {
+	const spinner = ora(`Building ${chalk.greenBright(name)}`).start()
+	const command = spawn('pod', ['repo', 'push', 'UniSpecs', `${name}.spec`, '--verbose', '--allow-warnings'])
+	const keyWords = [
+		'Testing with `xcodebuild`',
+		"Updating the `UniSpecs' repo",
+		"Adding the spec to the `UniSpecs' repo",
+		"Pushing the `UniSpecs' repo",
+	]
+	command.stdout.on('data', (data: Buffer) => {
+		const msg = data.toString()
+		if (keyWords.some((keyWord) => msg.includes(keyWord))) {
+			spinner.text = chalk.blueBright(msg)
+		}
+	})
+	command.on('error', (error) => spinner.fail(chalk.red(error.message)))
+	command.on('close', () => spinner.succeed(`Pushed ${chalk.cyanBright(name)} successfully!🎉🎉🎉`))
+}
+
+export default push
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import create from './command/create.js'
 import newProject from './command/new.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import push from './command/push.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -24,15 +25,18 @@ const program = new Command()
 program.version('0.0.1').description(chalk.greenBright('CLI For Managing Pods')).parse(process.argv)
 
 program
-  .command('lib <name>')
-  .description('create a swift pod')
-  .action((name: string) => create(name))
+	.command('lib <name>')
+	.description('create a swift pod')
+	.action((name: string) => create(name))
 
 program
-  .command('new <name>')
-  .description('new xcode project')
-  .action((name: string) => newProject(tplPath, name))
+	.command('new <name>')
+	.description('new xcode project')
+	.action((name: string) => newProject(tplPath, name))
 
-// program.command('tag').description('tag').action(tag)
+program
+	.command('push <name>')
+	.description('push pod to repo')
+	.action((name: string) => push(name))
 
 program.parse(process.argv)
