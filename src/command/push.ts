4311e80a8ec4111d5a1e7d5ca549458ed81a3568
FeliksLv <felikslv@163.com>
2024-10-23 00:59:48 +0800

    feat: 添加 push

--- src/command/push.ts
+++ src/command/push.ts
@@ -1,24 +1,38 @@
-import chalk from 'chalk'
-import ora from 'ora'
-import { spawn } from 'child_process'
+import chalk from 'chalk';
+import ora from 'ora';
+import { spawn } from 'child_process';
 
 const push = (name: string) => {
-	const spinner = ora(`Building ${chalk.greenBright(name)}`).start()
-	const command = spawn('pod', ['repo', 'push', 'UniSpecs', `${name}.spec`, '--verbose', '--allow-warnings'])
+	if (!name) {
+		console.log('error: name is null');
+		return;
+	}
+	const spinner = ora(`Building ${chalk.greenBright(name)}`).start();
+	const command = spawn('pod', [
+		'repo',
+		'push',
+		'UniSpecs',
+		`${name}.podspec`,
+		'--verbose',
+		'--allow-warnings',
+		'--use-libraries',
+		'--use-modular-headers',
+		'--skip-tests',
+	]);
 	const keyWords = [
 		'Testing with `xcodebuild`',
 		"Updating the `UniSpecs' repo",
 		"Adding the spec to the `UniSpecs' repo",
 		"Pushing the `UniSpecs' repo",
-	]
+	];
 	command.stdout.on('data', (data: Buffer) => {
-		const msg = data.toString()
+		const msg = data.toString();
 		if (keyWords.some((keyWord) => msg.includes(keyWord))) {
-			spinner.text = chalk.blueBright(msg)
+			spinner.text = chalk.blueBright(msg);
 		}
-	})
-	command.on('error', (error) => spinner.fail(chalk.red(error.message)))
-	command.on('close', () => spinner.succeed(`Pushed ${chalk.cyanBright(name)} successfully!🎉🎉🎉`))
-}
+	});
+	command.on('error', (error) => spinner.fail(chalk.red(error.message)));
+	command.on('close', () => spinner.succeed(`Pushed ${chalk.cyanBright(name)} successfully!🎉🎉🎉`));
+};
 
-export default push
+export default push;
